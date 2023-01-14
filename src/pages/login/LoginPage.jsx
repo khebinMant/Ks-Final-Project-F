2d0dcc9b1a34c36c62b8f267873aa352ad09ccbf
KenanAljaber <keno12333@hotmail.com>
2023-01-14 10:19:11 -0500

    adding the current user functionality

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,67 +1,149 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux"
-import { setUsers } from "../../store/user/userSlice";
+import { addUser, setCurrentUser, setUsers } from "../../store/user/userSlice";
 
 
 export const LoginPage = () => {
-  const dispatch =useDispatch();
-  const users= useSelector((state)=>state.users.users);
-console.log("here");
+  const dispatch = useDispatch();
+  const users = useSelector((state) => state.users.users);
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [name, setName] = useState("");
   
-  useEffect( () => {
+
+
+  useEffect(() => {
     //si hay usuarios guardados in el local storage los ponemos in el redux store
-		if (localStorage.getItem("users")) {
-			const localUsers =  JSON.parse(localStorage.getItem("users"));
+    if (localStorage.getItem("users")) {
+      const localUsers = JSON.parse(localStorage.getItem("users"));
       dispatch(setUsers(localUsers))
+      console.log("called");
 
-		}
-    //caso contrario no hay usuarios
-    else{
-			dispatch(setUsers([]))
-			setUsers([]);
-		}
-	}, [])
-
-
-function setCurrentUser(){
-
-}
-
-//este method busca en los usuarios guardados si hay match para el email y la contraseña ingresados para logearse
-function checkLogin(email, pass) {
-  let currUser = null;
-  users.forEach(it => {
-    if (it.email.toLowerCase() === email && it.pass === pass) {
-      currUser = it;
     }
-  });
-  return currUser;
+    //caso contrario no hay usuarios
+    else {
+      dispatch(setUsers([]))
+      setUsers([]);
+    }
+  }, [])
+
+
+/**
+	 * 	este method validra si el correo ingresado existe o no
+	 * @param {el correo ingresado del usuairo} email 
+	 * @returns true si el correo exite y false si no
+	 */
+function chekcUserExists(email) {
+  if(users.length>0){
+  return users.find(it => it.email.toLowerCase() === email.toLowerCase());
+}
+return false;
 }
 
-const login = (email,pass) => {
-  const currentUser = checkLogin(email, pass);
-  if (currentUser) {
-    dispatch(setCurrentUser(currentUser));
-    localStorage.setItem("currentUser", JSON.stringify(currentUser));
-  } else{
-    //mostrar un mensaje de error que no hay nigun usuario con los datos ingresados
-  } 
+  //este method busca en los usuarios guardados si hay match para el email y la contraseña ingresados para logearse
+  function checkLogin(email, pass) {
+    let currUser = null;
+    users.forEach(it => {
+      if (it.email.toLowerCase() === email && it.pass === pass) {
+        currUser = it;
+      }
+    });
+    return currUser;
+  }
 
-}
 
 
-function validateUser() {
-  //validar si el usuario que se esta inscribiendo tiene toda la info correcta
+  const login = (email, pass) => {
+    const currentUser = checkLogin(email, pass);
+    /*const currentUser = {
+      email: "demo@hotmail.com",
+      pass: "123456"
+    };*/
+    if (currentUser) {
+      dispatch(setCurrentUser(currentUser))
+      localStorage.setItem("currentUser", JSON.stringify(currentUser));
+    } else {
+      console.log("user does not exist");
+      //mostrar un mensaje de error que no hay nigun usuario con los datos ingresados
+    }
+  }
 
-  //retornar true si estan correctas y false en el otro caso
-}
+
+  //este method despues de llamar la method validateUser, en el caso que los datos son validos ingresera al usuario y lo agrega al la lista de usuarios
+  function signUp(){
+    const valid = validateUser();
+    
+    if (valid) {
+      const user = {
+        name: name,
+        email: email,
+        pass: pass
+      }
+      if (!chekcUserExists(user.email)) {
+       // showWarning("Successfully registered")
+        dispatch(addUser(user));
+        
+       console.log(users);
+       //setUsers(props.userReducer.users);
+        localStorage.setItem("users", JSON.stringify(users));
+        setName("");
+        setEmail("");
+        setPass("");
+        
+
+
+      }
+      else {
+        console.log("User already exists ",users);
+        //showWarning("email is already registered");
+      }
+
+    } else {
+      //showWarning("please fill the fields with correct info");
+    }
+
+
+  }
+
+
+
+
+  function handleOnChange(e) {
+    switch (e.target.name) {
+      case "emailInput":
+        setEmail(e.target.value);
+        break;
+      case "nameInput":
+        setName(e.target.value);
+        break;
+      case "passInput":
+        setPass(e.target.value);
+        break;
+    }
+
+  }
+
+  function validateUser() {
+    //validar si el usuario que se esta inscribiendo tiene toda la info correcta
+    
+    //retornar true si estan correctas y false en el otro caso
+
+    return true;
+  }
 
 
 
   return (
     <div>
-<h1 style={{color:"white"}}>asdasd</h1>
-
+      <h1 style={{ color: "white" }}>asdasd</h1>
+      <button onClick={login}>Login</button>
+      <div>
+        <h1>sign up mock</h1>
+        <input name="emailInput" type="text" value={email} onChange={handleOnChange} /><br />
+        <input name="nameInput" type="text" value={name} onChange={handleOnChange} /><br />
+        <input name="passInput" type="password" value={pass} onChange={handleOnChange} /><br />
+        <button onClick={signUp}>sign up</button>
+      </div>
     </div>
   )
 }
