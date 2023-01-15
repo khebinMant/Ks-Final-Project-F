45b027a66fabe9c8d0156f319fb18da7f560d0e0
khabLexander <mantillagka@hotmail.com>
2023-01-15 12:38:26 -0500

    fix

--- src/helpers/orders/postOrder.js
+++ src/helpers/orders/postOrder.js
@@ -1,6 +1,10 @@
+import { useSelector } from "react-redux";
 import { ordersApi } from "../../api/ordersApi"
 
 export const postOrder= async (order,customerId=1)=>{
+    
+    // const currentUser = useSelector((state) => state.users.currentUser);
+
     try{
         //mando quemado del user 1 hasta que kenan tenga el contexto de usuario  logeado
         const resp = await ordersApi.post(`/orders/user/${customerId}`,order)
